d5d1d6248aeae2c32a8bc5933f49e6ae3029ecf1
Hauwa Ibrahim <umeey25@gmail.com>
2025-03-01 01:23:39 +0100

    Enhance QR code generator with improved styles and functionality

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import qrcode
+import os
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
+    # Remove the existing QR code image if it exists
+    qr_image_path = 'static/image.jpg'
+    if os.path.exists(qr_image_path):
+        os.remove(qr_image_path)
     return render_template('index.html')
-@app.route('/makeQr', methods=['GET', 'POST'])
+
+@app.route('/makeQr', methods=['POST'])
 def make():
-    data = request.form.to_dict()
-    data_to_convert = data['data']
-    qr = qrcode.make(data_to_convert)
-    qr.save('static/image.jpg')
+    try:
+        data = request.form['data']
+        if data:
+            qr = qrcode.make(data)
+            qr.save('static/image.jpg')
+        else:
+            return render_template('index.html', error="No data provided.")
+    except Exception as e:
+        return render_template('index.html', error=str(e))
     return render_template('index.html')
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
